43e70df06c360dbe820847dc2ff807b66a8621f0
Olim2508 <rahmatovolim3@gmail.com>
2022-08-03 21:22:50 +0500

    set up static settings

--- src/settings.py
+++ src/settings.py
@@ -146,6 +146,10 @@ LOGOUT_URL = 'rest_framework:logout'
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
--- src/urls.py
+++ src/urls.py
@@ -13,6 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.auth.decorators import login_required
 from django.urls import path, include
@@ -29,3 +31,7 @@ urlpatterns = [
 ]
 
 urlpatterns += swagger_url
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
