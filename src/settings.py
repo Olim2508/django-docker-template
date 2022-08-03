d8d0fa52d0f7ad890daa7d34df8b2a5951e0c6c9
Olim2508 <rahmatovolim3@gmail.com>
2022-08-03 13:23:44 +0500

    added swagger link in admin panel

--- src/settings.py
+++ src/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 from .additional_settings.jwt_settings import *
 # from .additional_settings.swagger_settings import *
@@ -79,7 +79,7 @@ ROOT_URLCONF = 'src.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
